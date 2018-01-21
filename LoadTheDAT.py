9c101abb7e5fcd26516b1144571b89f6ea758616
cenbc <qqww3474@126.com>
2018-01-21 20:56:09 +0800

    读取.DAT文件，采用pandas的dataframe，直接筛选出高中低端有功，可以读取288个点。

--- LoadTheDAT.py
+++ LoadTheDAT.py
@@ -2,6 +2,7 @@
 from TimeToName import time_name
 import time
 import pandas as pd
+import datetime
 
 name = time_name(2017, 12, 16)
 CZ = []         # 厂站
@@ -11,7 +12,7 @@ ZHI = []  # 值
 Path = r'D:\Python Study\利用Python读取EMS数据\01 20171217EMS数据'
 
 start = time.clock()
-range_end = 100
+range_end = 288
 for i in range(0, range_end):
     print(i)
     file = Path + '\EMS_15M_' + name[i] + '.dat'
@@ -24,33 +25,50 @@ for i in range(0, range_end):
     if i == 0:
         for x in line1:
             line2.append(x.split(','))      # append方法表示从列表最后新增元素
-            CZ.append([line2[-1][1]])         # 厂站位于第二列，Python计数从0开始
-            WLL.append([line2[-1][2]])        # 物理量位于第三列
+            CZ.append([line2[-1][1] + ',' + line2[-1][2]])         # 厂站位于第二列，Python计数从0开始，物理量位于第三列
             ZHI.append(list(map(float, [line2[-1][-1]])))       # 值在最后一列
     else:
         j = 0
         for x in line1:
             line2.append(x.split(','))  # append方法表示从列表最后新增元素
-            CZ[j].append(line2[-1][1])  # 厂站位于第二列，Python计数从0开始
-            WLL[j].append(line2[-1][2])  # 物理量位于第三列
+            CZ[j].append(line2[-1][1] + ',' + line2[-1][2])  # 厂站位于第二列，Python计数从0开始
             ZHI[j].append(float(line2[-1][-1]))  # 值在最后一列
             j = j + 1
     EMS1.close()
 
-# CZ = pd.DataFrame(CZ)
-# WLL = pd.DataFrame(WLL)
-ZHI = pd.DataFrame(ZHI)
 
+CZ = pd.DataFrame(CZ)
+ZHI = pd.DataFrame(ZHI, index=CZ[0], columns=name[:range_end])
+gao = ZHI.loc[ZHI.index.str.contains('高端有功$')]
+zhong = ZHI.loc[ZHI.index.str.contains('中端有功$')]
+di = ZHI.loc[ZHI.index.str.contains('低端有功$')]
+elapsed = (time.clock() - start)
+print("Time used:", elapsed)
 
+mid = time.clock()
 writer = pd.ExcelWriter('try.xlsx')
-# CZ.to_excel(writer, '厂站')
-# WLL.to_excel(writer, '物理量')
-ZHI.to_excel(writer, '值')
+# ZHI.to_excel(writer, '值')
+# elapsed = (time.clock() - mid)
+# print("写入值所需时间:", elapsed)
+
+gao.to_excel(writer, '高端有功')
+elapsed = (time.clock() - mid)
+print("写入高端所需时间:", elapsed)
+
+mid = time.clock()
+zhong.to_excel(writer, '中端有功')
+elapsed = (time.clock() - mid)
+print("写入中端所需时间:", elapsed)
+
+mid = time.clock()
+di.to_excel(writer, '低端有功')
+elapsed = (time.clock() - mid)
+print("写入低端所需时间:", elapsed)
+
 writer.save()
 
 
 elapsed = (time.clock() - start)
-
 print("Time used:", elapsed)
 # print(CZ)
 # print(WLL)
