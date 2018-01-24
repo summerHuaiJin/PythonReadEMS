3f8a5862926fb502f1b4d4283f24ee1c061f0036
cenbc <qqww3474@126.com>
2018-01-24 16:53:22 +0800

    读取.DAT文件，采用pandas的dataframe，直接筛选出高中低端有功，可以读取288个点。优化了循环内结构，不再生出二维列表，而是用一维列表直接填入DataFrame中

--- LoadTheDAT.py
+++ LoadTheDAT.py
@@ -2,7 +2,6 @@
 from TimeToName import time_name
 import time
 import pandas as pd
-import datetime
 
 name = time_name(2017, 12, 16)
 CZ = []         # 厂站
@@ -12,8 +11,24 @@ ZHI = []  # 值
 Path = r'D:\Python Study\利用Python读取EMS数据\01 20171217EMS数据'
 
 start = time.clock()
-range_end = 288
-for i in range(0, range_end):
+range_end = 10
+
+print(0)
+file = Path + '\EMS_15M_' + name[0] + '.dat'
+EMS1 = open(file, 'r')
+line = EMS1.read()
+line = line.splitlines()
+line1 = line[2:-1]  # 去掉前两行和最后一行
+
+for x in line1:
+    line2.append(x.split(','))  # append方法表示从列表最后新增元素
+    CZ.append(line2[-1][1] + ',' + line2[-1][2])  # 厂站位于第二列，Python计数从0开始，物理量位于第三列
+    ZHI.append(float(line2[-1][-1]))  # 值在最后一列
+CZ1 = CZ
+ZHI1 = pd.DataFrame(ZHI, index=CZ, columns=[name[0]])
+print(ZHI1)
+
+for i in range(1, range_end):
     print(i)
     file = Path + '\EMS_15M_' + name[i] + '.dat'
     EMS1 = open(file, 'r')
@@ -22,34 +37,24 @@ for i in range(0, range_end):
     line1 = line[2:-1]      # 去掉前两行和最后一行
     line2 = []
 
-    if i == 0:
-        for x in line1:
-            line2.append(x.split(','))      # append方法表示从列表最后新增元素
-            CZ.append([line2[-1][1] + ',' + line2[-1][2]])         # 厂站位于第二列，Python计数从0开始，物理量位于第三列
-            ZHI.append(list(map(float, [line2[-1][-1]])))       # 值在最后一列
-    else:
-        j = 0
-        for x in line1:
-            line2.append(x.split(','))  # append方法表示从列表最后新增元素
-            CZ[j].append(line2[-1][1] + ',' + line2[-1][2])  # 厂站位于第二列，Python计数从0开始
-            ZHI[j].append(float(line2[-1][-1]))  # 值在最后一列
-            j = j + 1
+    CZ = []
+    ZHI = []
+    for x in line1:
+        line2.append(x.split(','))  # append方法表示从列表最后新增元素
+        CZ.append(line2[-1][1] + ',' + line2[-1][2])         # 厂站位于第二列，Python计数从0开始，物理量位于第三列
+        ZHI.append(float(line2[-1][-1]))            # 值在最后一列
+    ZHI1[name[i]] = ZHI
     EMS1.close()
+print(ZHI1)
 
-
-CZ = pd.DataFrame(CZ)
-ZHI = pd.DataFrame(ZHI, index=CZ[0], columns=name[:range_end])
-gao = ZHI.loc[ZHI.index.str.contains('高端有功$')]
-zhong = ZHI.loc[ZHI.index.str.contains('中端有功$')]
-di = ZHI.loc[ZHI.index.str.contains('低端有功$')]
+gao = ZHI1.loc[ZHI1.index.str.contains('高端有功$')]
+zhong = ZHI1.loc[ZHI1.index.str.contains('中端有功$')]
+di = ZHI1.loc[ZHI1.index.str.contains('低端有功$')]
 elapsed = (time.clock() - start)
 print("Time used:", elapsed)
 
 mid = time.clock()
 writer = pd.ExcelWriter('try.xlsx')
-# ZHI.to_excel(writer, '值')
-# elapsed = (time.clock() - mid)
-# print("写入值所需时间:", elapsed)
 
 gao.to_excel(writer, '高端有功')
 elapsed = (time.clock() - mid)
@@ -67,10 +72,5 @@ print("写入低端所需时间:", elapsed)
 
 writer.save()
 
-
 elapsed = (time.clock() - start)
 print("Time used:", elapsed)
-# print(CZ)
-# print(WLL)
-# print(ZHI)
-# print(line2[-1])
