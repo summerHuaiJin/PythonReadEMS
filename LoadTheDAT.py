f7bee306132976dc51aef5f92b2d9b09278476ae
cenbc <qqww3474@126.com>
2018-01-31 16:48:56 +0800

    读取.DAT文件，V2.0版本，可以运行，厂站名由于采用了DataFrame的join方法，保证了每次都能够对应，增加了读取无功的功能

--- LoadTheDAT.py
+++ LoadTheDAT.py
@@ -3,15 +3,23 @@ from TimeToName import time_name
 import time
 import pandas as pd
 
-start = time.clock()
-name = time_name(2017, 12, 16)
+start = time.clock()    # 记录程序开始的时间
+name = time_name(2017, 12, 16)  # 调用time_name程序
 CZ = []         # 厂站
 line2 = []
-WLL = []  # 物理量（即电压、电流、有功等）
 ZHI = []  # 值
 Path = r'D:\Python Study\利用Python读取EMS数据\01 20171217EMS数据'
 
-range_end = 288
+txtFile = '有功无功.txt'    # 这个txt文件是所要读取的厂站信息
+txt = open(txtFile, encoding='UTF-8')   # 打开这个txt文件，采用的编码形式是UTF-8
+nameOfPower1 = txt.read()   # read方法表示读取文件里的所有内容
+nameOfPower1 = nameOfPower1.splitlines()    # splitlines方法表示根据\n来分行，并且去掉\n
+nameOfPower = list(set(nameOfPower1))       # set是集合方法，是一个无序无重复值的集合，用来去掉重复值；set后返回的是集合
+nameOfPower.sort(key=nameOfPower1.index)    # sort方法是排序，key表示按照何种方法排序
+ZHI1 = pd.DataFrame(index=nameOfPower)      # DataFrame是建立一个类似矩阵的东西，index表示其索引，内容为空
+txt.close()     # 关闭所打开的txt文件
+
+range_end = 288     # 需要读取的时间点数，一天288个点
 
 print(0)
 file = Path + '\EMS_15M_' + name[0] + '.dat'
@@ -21,15 +29,20 @@ line = line.splitlines()
 line1 = line[2:-1]  # 去掉前两行和最后一行
 
 for x in line1:
-    line2.append(x.split(','))  # append方法表示从列表最后新增元素
-    CZ.append(line2[-1][1] + ',' + line2[-1][2])  # 厂站位于第二列，Python计数从0开始，物理量位于第三列
-    ZHI.append(float(line2[-1][-1]))  # 值在最后一列
-CZ1 = CZ
-ZHI1 = pd.DataFrame(ZHI, index=CZ, columns=[name[0]])
+    line2.append(x.split(','))  # append方法表示从列表最后新增元素，split按逗号分隔
+    temp = line2[-1][0] + ',' + line2[-1][1] + ',' + line2[-1][2]   # 厂站+物理量
+    CZ.append(temp)
+    ZHI.append(float(line2[-1][-1]))  # 值在最后一列，float表示转化为浮点数
+y = pd.DataFrame(ZHI, index=CZ, columns=[name[0]])  # 创建一个DataFrame
+y = y.loc[nameOfPower]  # loc方法表示索引按照nameOfPower来取数
+
+dIndex = y.index.duplicated()   # duplicated方法表示找出重复值，返回true或者false
+y = y[~dIndex]  # ~表示取反
+ZHI1 = ZHI1.join(y, how='left')     # join方法是将y按照索引值加入到ZHI1之中，加入的方式left表示以ZHI1的索引为准
 
 for i in range(1, range_end):
     print(i)
-    file = Path + '\EMS_15M_' + name[i] + '.dat'
+    file = Path + '\EMS_15M_' + name[i] + '.dat'    # 依次打开文件
     EMS1 = open(file, 'r')
     line = EMS1.read()
     line = line.splitlines()
@@ -40,14 +53,22 @@ for i in range(1, range_end):
     ZHI = []
     for x in line1:
         line2.append(x.split(','))  # append方法表示从列表最后新增元素
-        CZ.append(line2[-1][1] + ',' + line2[-1][2])         # 厂站位于第二列，Python计数从0开始，物理量位于第三列
-        ZHI.append(float(line2[-1][-1]))            # 值在最后一列
-    ZHI1[name[i]] = ZHI
+        CZ.append(line2[-1][0] + ',' + line2[-1][1] + ',' + line2[-1][2])  # 厂站+物理量
+        ZHI.append(float(line2[-1][-1]))    # 值在最后一列
+    y = pd.DataFrame(ZHI, index=CZ, columns=[name[i]])
+    y = y.loc[nameOfPower]
+    dIndex = y.index.duplicated()
+    y = y[~dIndex]
+    ZHI1 = ZHI1.join(y, how='left')
     EMS1.close()
 
-gao = ZHI1.loc[ZHI1.index.str.contains('高端有功$')]
+gao = ZHI1.loc[ZHI1.index.str.contains('高端有功$')]    # DataFrame.index.str.contains()可以处理正则表达式
 zhong = ZHI1.loc[ZHI1.index.str.contains('中端有功$')]
 di = ZHI1.loc[ZHI1.index.str.contains('低端有功$')]
+
+gaoWu = ZHI1.loc[ZHI1.index.str.contains('高端无功$')]
+zhongWu = ZHI1.loc[ZHI1.index.str.contains('中端无功$')]
+diWu = ZHI1.loc[ZHI1.index.str.contains('低端无功$')]
 elapsed = (time.clock() - start)
 print("Time used:", elapsed)
 
@@ -56,17 +77,37 @@ writer = pd.ExcelWriter('try.xlsx')
 
 gao.to_excel(writer, '高端有功')
 elapsed = (time.clock() - mid)
-print("写入高端所需时间:", elapsed)
+print("写入高端有功所需时间:", elapsed)
 
 mid = time.clock()
 zhong.to_excel(writer, '中端有功')
 elapsed = (time.clock() - mid)
-print("写入中端所需时间:", elapsed)
+print("写入中端有功所需时间:", elapsed)
 
 mid = time.clock()
 di.to_excel(writer, '低端有功')
 elapsed = (time.clock() - mid)
-print("写入低端所需时间:", elapsed)
+print("写入低端有功所需时间:", elapsed)
+
+mid = time.clock()
+gaoWu.to_excel(writer, '高端无功')
+elapsed = (time.clock() - mid)
+print("写入低端无功所需时间:", elapsed)
+
+mid = time.clock()
+zhongWu.to_excel(writer, '中端无功')
+elapsed = (time.clock() - mid)
+print("写入中端无功所需时间:", elapsed)
+
+mid = time.clock()
+diWu.to_excel(writer, '低端无功')
+elapsed = (time.clock() - mid)
+print("写入低端无功所需时间:", elapsed)
+
+mid = time.clock()
+ZHI1.to_excel(writer, '总')
+elapsed = (time.clock() - mid)
+print("写入所有的有功无功所需时间:", elapsed)
 
 mid = time.clock()
 writer.save()
